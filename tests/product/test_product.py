e4c6eb5558729963dd0e932bd8f4de291041398e
IgorHamzi <igorwhamzi@gmail.com>
2022-10-31 19:59:03 -0300

    test: add test for Product attributes

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,12 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass
+    product = Product()
+    assert type(product.id) == int
+    assert type(product.nome_do_produto) == str
+    assert type(product.nome_da_empresa) == str
+    assert type(product.data_de_fabricacao) == str
+    assert type(product.data_de_validade) == str
+    assert type(product.numero_de_serie) == str
+    assert type(product.instrucoes_de_armazenamento) == str
