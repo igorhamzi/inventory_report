fa5cf96748eec9738c26c577b8814d06c7f7c42c
IgorHamzi <igorwhamzi@gmail.com>
2022-11-01 17:17:32 -0300

    feat: add simple report

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from collections import Counter
+
+
+class SimpleReport:
+
+    @classmethod
+    def generate(cls, list):
+        oldest_manufacturing_date = min([product['data_de_fabricacao']
+                                        for product in list
+                                         ])
+
+        current_day = str(datetime.today())
+
+        closest_expiration_date = min([product['data_de_validade']
+                                      for product in list
+                                      if product['data_de_validade'] >
+                                      current_day])
+
+        companies = [product['nome_da_empresa']
+                     for product in list
+                     ]
+
+        company_more_products = Counter(companies).most_common(1)[0][0]
+
+        print(oldest_manufacturing_date)
+        print(closest_expiration_date)
+        print(company_more_products)
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_more_products}"
+        )
+
+
+if __name__ == '__main__':
+    SimpleReport.generate(
+        '/home/igor/trybe/projetos-19'
+        '/cs-modulo/inventory-report'
+        '/sd-019-c-inventory-report'
+        '/inventory_report/data/inventory.json'
+    )
+
+    print(__name__)
