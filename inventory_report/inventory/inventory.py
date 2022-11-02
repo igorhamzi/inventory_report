85316b507a44b9c85939614fc99452e60be922d8
IgorHamzi <igorwhamzi@gmail.com>
2022-11-01 22:33:24 -0300

    feat: add json file read

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,23 @@
 from csv import DictReader
 from ..reports.simple_report import SimpleReport
 from ..reports.complete_report import CompleteReport
+import json
 
 
 class inventory:
 
     @classmethod
     def import_data(cls, path, inventory):
-        with open(path, 'r') as file:
-            list_of_products = list(DictReader(file))
+        file_type = path[-4::]
+
+        if file_type == '.csv':
+            with open(path, 'r') as file:
+                list_of_products = list(DictReader(file))
+        elif file_type == 'json':
+            with open(path, 'r') as file:
+                list_of_products = json.load(file)
+
+        print(list_of_products)
 
         if inventory == 'simples':
             return SimpleReport.generate(list_of_products)
@@ -21,7 +30,7 @@ if __name__ == '__main__':
         '/home/igor/trybe/projetos-19'
         '/cs-modulo/inventory-report'
         '/sd-019-c-inventory-report'
-        '/inventory_report/data/inventory.csv',
+        '/inventory_report/data/inventory.json',
         'completo'
     )
 
