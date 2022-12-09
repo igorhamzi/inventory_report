baf84dabd351b4550405e89f6ecd14d0b439818f
IgorHamzi <igorwhamzi@gmail.com>
2022-12-09 20:21:13 -0300

    feat: add return of reports

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,26 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+    _, path, type = sys.argv
+
+    if path.endswith(".csv"):
+        return sys.stdout.write(
+            InventoryRefactor(CsvImporter).import_data(path, type)
+        )
+    if path.endswith(".json"):
+        return sys.stdout.write(
+            InventoryRefactor(JsonImporter).import_data(path, type)
+        )
+
+    if path.endswith(".xml"):
+        return sys.stdout.write(
+            InventoryRefactor(XmlImporter).import_data(path, type)
+        )
